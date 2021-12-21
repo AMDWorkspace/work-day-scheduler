3c6bb8cdcd1e13741d0e88024c6d189a13c41c83
Alan Do <amdworkspace@gmail.com>
2021-12-21 00:10:58 -0500

    added text input into local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,55 +1,97 @@
-var time = moment();
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
-function setPlanner() {
-    // Date function from moment.js:
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+var tasks = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+};
 
-    // To retrieve the schedule-field text from localStorage:
-        $(".time-block").each(function () {
-            var id = $(this).attr("id");
-            var schedule = JSON.parse(localStorage.getItem("schedule"));
-    
-            if (schedule !== null) {
-                $(this).children(".description").val(schedule);
-            }
-        });
-    }
-setPlanner();
-
-// Saves to localStorage:
-var saveBtn = $(".saveBtn");
-    
-
-saveBtn.on("click", function () {
-    var time = $(this).parent().attr("id");
-    var schedule = $(this).siblings(".description").val().trim();
-    
-    localStorage.setItem("time", JSON.stringify(time));
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-
-    localStorage.getItem("time");
-    localStorage.getItem("schedule");
-});
-
-// Function for changing from past to present to future on the grid:
-function pastPresentFuture() {
-    hour = time.hours();
-    $(".time-block").each(function () {
-        var thisHour = parseInt($(this).attr("data-time"));
-
-        if (thisHour > hour) {
-            $(this).addClass("future")
-        }
-        else if (thisHour === hour) {
-            $(this).addClass("present");
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-        }
-    })
+var setTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-pastPresentFuture();
+var getTasks = function() {
+
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadedTasks) {
+        tasks = loadedTasks
+
+        $.each(tasks, function(hour, task) {
+            var hourDiv = $("#" + hour);
+            createTask(task, hourDiv);
+        })
+    }
+
+    auditTasks();
+}
+
+var createTask = function(taskText, hourDiv) {
+
+    var taskDiv = hourDiv.find(".task");
+    var taskP = $("<p>").addClass("description").text(taskText)
+        taskDiv.html(taskP);
+}
+
+var auditTasks = function() {
+
+    var currentHour = moment().hour();
+    $(".task-info").each( function() {
+        var elementHour = parseInt($(this).attr("id"));
+
+        if ( elementHour < currentHour ) {
+            $(this).removeClass(["present", "future"]).addClass("past");
+        }
+        else if ( elementHour === currentHour ) {
+            $(this).removeClass(["past", "future"]).addClass("present");
+        }
+        else {
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+    })
+};
+
+var replaceTextarea = function(textareaElement) {
+    var taskInfo = textareaElement.closest(".task-info");
+    var textArea = taskInfo.find("textarea");
+    var time = taskInfo.attr("id");
+    var text = textArea.val().trim();
+
+    tasks[time] = [text]; 
+    setTasks();
+
+    createTask(text, taskInfo);
+}
+
+$(".task").click(function() {
+
+    $("textarea").each(function() {
+        replaceTextarea($(this));
+    })
+
+    var time = $(this).closest(".task-info").attr("id");
+    if (parseInt(time) >= moment().hour()) {
+        var text = $(this).text();
+        var textInput = $("<textarea>").addClass("form-control").val(text);
+
+        $(this).html(textInput);
+        textInput.trigger("focus");
+    }
+})
+
+$(".saveBtn").click(function() {
+    replaceTextarea($(this));
+})
+
+timeToHour = 3600000 - today.milliseconds();
+setTimeout(function() {
+    setInterval(auditTasks, 3600000)
+}, timeToHour);
+
+getTasks();
